66321f4ebd377dcbb6c6218aa785991deb54aa3a
Dominik Werthmueller <werthmuellerd@gmail.com>
2019-02-03 15:04:27 +0000

    FAEventT: added members for number of (MC) particles

--- include/FAEventT.h
+++ include/FAEventT.h
@@ -23,10 +23,13 @@ class FAEventT : public FAEventBase
 {
 
 public:
+    Short_t nPart;                      // number of particles
+    Short_t nPartMC;                    // number of MC particles
     std::vector<PartType> part;         // array of particles
     std::vector<PartTypeMC> partMC;     // array of particles
 
-    FAEventT() : FAEventBase() { }
+    FAEventT() : FAEventBase(),
+                 nPart(0), nPartMC(0) { }
     FAEventT(const FAEventT& orig);
     virtual ~FAEventT() { }
 
@@ -49,6 +52,8 @@ FAEventT<PartType, PartTypeMC>::FAEventT(const FAEventT& orig)
     // Copy constructor.
 
     // init members
+    nPart = orig.nPart;
+    nPartMC = orig.nPartMC;
     part = orig.part;
     partMC = orig.partMC;
 }
@@ -60,6 +65,7 @@ void FAEventT<PartType, PartTypeMC>::AddParticle(PartType& p)
     // Add a particle to the list of particles.
 
     part.push_back(p);
+    nPart++;
 }
 
 //______________________________________________________________________________
@@ -69,6 +75,7 @@ void FAEventT<PartType, PartTypeMC>::AddParticleMC(PartTypeMC& p)
     // Add a particle to the list of MC particles.
 
     partMC.push_back(p);
+    nPartMC++;
 }
 
 //______________________________________________________________________________
@@ -78,14 +85,14 @@ void FAEventT<PartType, PartTypeMC>::Print(Option_t* option) const
     // Print the content of this class.
 
     FAEventBase::Print(option);
-    printf("Number of particles    : %ld\n", part.size());
-    for (UInt_t i = 0; i < part.size(); i++)
+    printf("Number of particles    : %d\n", nPart);
+    for (Int_t i = 0; i < nPart; i++)
     {
         printf("-> Particle %d\n", i+1);
         part[i].Print(option);
     }
-    printf("Number of MC particles : %ld\n", partMC.size());
-    for (UInt_t i = 0; i < partMC.size(); i++)
+    printf("Number of MC particles : %d\n", nPartMC);
+    for (Int_t i = 0; i < nPartMC; i++)
     {
         printf("-> MC Particle %d\n", i+1);
         partMC[i].Print(option);
@@ -99,6 +106,8 @@ void FAEventT<PartType, PartTypeMC>::Clear(Option_t* option)
     // Prepare class for a new event by clearing all members.
 
     FAEventBase::Clear(option);
+    nPart = 0;
+    nPartMC = 0;
     part.clear();
     partMC.clear();
 }
@@ -113,6 +122,8 @@ FAEventT<PartType, PartTypeMC>& FAEventT<PartType, PartTypeMC>::operator=(const 
     if (this != &e)
     {
         FAEventBase::operator=(e);
+        nPart = e.nPart;
+        nPartMC = e.nPartMC;
         part = e.part;
         partMC = e.partMC;
     }
