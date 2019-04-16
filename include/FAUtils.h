65a45477cb96dd0d18c200f4d0153a228909aa06
Dominik Werthmueller <werthmuellerd@gmail.com>
2019-04-16 17:33:37 +0100

    FAUtils: added template LoadObject()

--- include/FAUtils.h
+++ include/FAUtils.h
@@ -15,6 +15,9 @@
 #define FooAna_FAUtils
 
 #include "TDataType.h"
+#include "TFile.h"
+#include "TH1.h"
+#include "TError.h"
 
 class TLorentzVector;
 class TAxis;
@@ -36,6 +39,52 @@ namespace FAUtils
 
     void Calculate4Vector(Double_t theta, Double_t phi, Double_t t, Double_t mass,
                           TLorentzVector& p4);
+
+    template <class T>
+    Bool_t LoadObject(const Char_t* file, const Char_t* name, T*& out)
+    {
+        // Load generic TObjects named 'name' from the file 'file' to 'out'.
+
+        // set output object pointer to 0
+        out = 0;
+
+        // try to open the file
+        TString fileExp = ExpandPath(file);
+        TFile f(fileExp.Data());
+
+        // check if file exists
+        if (!f.IsZombie())
+        {
+            // try to load the object
+            f.GetObject(name, out);
+            if (out)
+            {
+                // clone the object
+                out = (T*)out->Clone();
+
+                // unset directory for histograms
+                if (out->InheritsFrom("TH1"))
+                {
+                    TH1* h = (TH1*) out;
+                    h->SetDirectory(0);
+                }
+
+                return kTRUE;
+            }
+            else
+            {
+                Error("FAUtils::LoadObject", "Could not load the object '%s'!", name);
+                return kFALSE;
+            }
+        }
+        else
+        {
+            // file opening error
+            Error("FAUtils::LoadObject", "Could not open the file '%s' containing the '%s' object!",
+                  fileExp.Data(), name);
+            return kFALSE;
+        }
+    }
 }
 
 #endif
