188d9322a140c5b1d725df5a06c3b6c672b759bf
Dominik Werthmueller <werthmuellerd@gmail.com>
2019-08-20 16:16:51 +0100

    FAConfigA2: new A2 detector bit pattern

--- include/FAConfigA2.h
+++ include/FAConfigA2.h
@@ -14,14 +14,35 @@
 #ifndef FooAna_FAConfigA2
 #define FooAna_FAConfigA2
 
+#include "Rtypes.h"
+
 namespace FAConfigA2
 {
-    enum EA2Detector {
-        kNoDetector,        // no detector
-        kCBDetector,        // Crystal Ball
-        kTAPSDetector,      // TAPS
-        kTaggerDetector     // photon tagger
+    // define A2 detectors
+    enum FADetectorA2_t
+    {
+        kEmpty   = 0,
+        kTagger  = 1 << 0,
+        kMicro   = 1 << 1,
+        kCB      = 1 << 2,
+        kPID     = 1 << 3,
+        kMWPC_1  = 1 << 4,
+        kMWPC_2  = 1 << 5,
+        kTAPS    = 1 << 6,
+        kVeto    = 1 << 7,
+        kPizza   = 1 << 8,
+        kTOFWall = 1 << 9
     };
+
+    // define binary operators for FADetectorA2_t
+    inline FADetectorA2_t operator|(const FADetectorA2_t a, const FADetectorA2_t b)
+    {
+        return (FADetectorA2_t) ((Int_t)a | (Int_t)b);
+    }
+    inline FADetectorA2_t& operator|=(FADetectorA2_t& a, const FADetectorA2_t b)
+    {
+        return (FADetectorA2_t&) ((Int_t&)a |= (Int_t)b);
+    }
 }
 
 #endif
