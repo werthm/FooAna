2675e34b92fee4de9484f0e2b05509935eea4733
Dominik Werthmueller <werthmuellerd@gmail.com>
2019-12-09 18:33:15 +0000

    FAFooAna: adding type FAVector3

--- include/FAFooAna.h
+++ include/FAFooAna.h
@@ -21,6 +21,8 @@
 
 // typedef
 typedef ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<Double32_t>> FAVector4;
+typedef ROOT::Math::DisplacementVector3D<ROOT::Math::Cartesian3D<Double32_t>,
+                                         ROOT::Math::DefaultCoordinateSystemTag> FAVector3;
 
 #endif
 
