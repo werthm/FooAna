d0f043b5264e62f83423d46503c6b835e49f7aca
Dominik Werthmueller <werthmuellerd@gmail.com>
2019-12-09 18:34:36 +0000

    FAParticleCLAS12_B: adding member for momentum

--- include/FAParticleCLAS12_B.h
+++ include/FAParticleCLAS12_B.h
@@ -16,21 +16,24 @@
 
 #include "FAConfigCLAS12.h"
 #include "FAUtilsCLAS12.h"
+#include "FAFooAna.h"
 
 class FAParticleCLAS12_B
 {
 
 public:
     FAConfigCLAS12::FADetectorCLAS12_t det; // detector bit map
-    Double32_t energy;                      // detected energy [GeV]
+    FAVector3 mom;                          // momentum [GeV]
     Double32_t time;                        // time [ns]
+    Double32_t energy;                      // detected energy [GeV]
     Double32_t deltaE;                      // detected deltaE [GeV]
     Double32_t betaTOF;                     // beta from time-of-flight
     Double32_t betaPID;                     // beta from momentum and PID
     Double32_t chi2PID;                     // chi2 PID
 
     FAParticleCLAS12_B() : det(FAConfigCLAS12::kNoDet),
-                           energy(0), time(0), deltaE(0),
+                           mom(0, 0, 0), time(0),
+                           energy(0), deltaE(0),
                            betaTOF(0), betaPID(0),
                            chi2PID(0) { }
     virtual ~FAParticleCLAS12_B() { }
@@ -38,8 +41,9 @@ public:
     virtual void Print(Option_t* option = "") const
     {
         printf("Detectors              : %s\n", FAUtilsCLAS12::DetectorsAsString(det).Data());
-        printf("Energy                 : %f\n", energy);
+        printf("Momentum               : %f, %f, %f\n", mom.X(), mom.Y(), mom.Z());
         printf("Time                   : %f\n", time);
+        printf("Energy                 : %f\n", energy);
         printf("DeltaE                 : %f\n", deltaE);
         printf("Beta TOF               : %f\n", betaTOF);
         printf("Beta PID               : %f\n", betaPID);
@@ -48,8 +52,9 @@ public:
     virtual void Clear(Option_t* option = "")
     {
         det = FAConfigCLAS12::kNoDet;
-        energy = 0;
+        mom.SetXYZ(0, 0, 0);
         time = 0;
+        energy = 0;
         deltaE = 0;
         betaTOF = 0;
         betaPID = 0;
